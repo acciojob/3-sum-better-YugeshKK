df92b4d72abfb8106dcc47d1662d6cfa0be99ceb
YugeshKK <95625392+YugeshKK@users.noreply.github.com>
2023-09-27 10:59:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function threeSum(arr, target) {
-// write your code here
-  
-}
+    // write your code here
+      for (let index = 0; index < arr.length; index++) {
+          let samp=[arr[index]];
+          let j=index;
+              while(samp.length<3){
+                    j++;
+                  samp[j]=arr[j];
+              }
+          let sum=0;
+            for (let m = 0; m < samp.length; m++) {
+                sum+=samp[m];
+            }
+          if(sum==target+1 || sum==target-1){
+                return sum;
+          }
+      }
+    }
 
 module.exports = threeSum;
